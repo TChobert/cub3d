0837e49ad37d74079df466f6af592ecbd7574aa8
racoutte <racoutte@student.42.fr>
2025-03-24 14:35:14 +0100

    modif naming map_file_path dans fonction exec_game_cub 24/03

--- srcs/game/launch_game.c
+++ srcs/game/launch_game.c
@@ -6,14 +6,14 @@
 /*   By: racoutte <racoutte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 11:08:12 by racoutte          #+#    #+#             */
-/*   Updated: 2025/03/24 14:00:28 by racoutte         ###   ########.fr       */
+/*   Updated: 2025/03/24 14:33:34 by racoutte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub_parsing.h"
 
-void	exec_game_cub(char *map_file)
+void	exec_game_cub(char *map_file_path)
 {
-	(void)map_file;
+	(void)map_file_path;
 	printf("Hello\n");
 }
