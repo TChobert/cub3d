36649a74f0b9f96585aa399ce119b0f259836bbd
racoutte <racoutte@student.42.fr>
2025-04-08 12:29:12 +0200

    ajout core_map_parser.c 08/04

--- srcs/parsing/map_file_parser/parse_map_file.c
+++ srcs/parsing/map_file_parser/parse_map_file.c
@@ -6,7 +6,7 @@
 /*   By: racoutte <racoutte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 17:42:54 by tchobert          #+#    #+#             */
-/*   Updated: 2025/04/07 17:01:34 by racoutte         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:24:52 by racoutte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ t_content_status	check_if_valid_map(t_map_data *map_data)
 {
 	if (check_if_valid_first_and_last_wall_line(map_data) == INVALID_MAP
 		|| check_if_valid_map_characters(map_data) == INVALID_MAP)
+	{
+		ft_dprintf(STDERR_FILENO, "Error\nInvalid map.\n");
+		return (INVALID_CONTENT);
+	}
+	if (core_map_parser(map_data) == INVALID_MAP)
 	{
 		ft_dprintf(STDERR_FILENO, "Error\nInvalid map.\n");
 		return (INVALID_CONTENT);
