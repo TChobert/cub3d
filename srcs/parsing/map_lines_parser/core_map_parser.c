c8360229d057fdeff058bc3ae3c266d480b698fb
Thomas Chobert <tchobert@paul-f3Ar5s7.clusters.42paris.fr>
2025-04-08 16:23:32 +0200

    mefiance avec valgrind passe

--- srcs/parsing/map_lines_parser/core_map_parser.c
+++ srcs/parsing/map_lines_parser/core_map_parser.c
@@ -12,18 +12,18 @@
 
 #include "cub_parsing.h"
 
-t_map_status	core_map_parser(t_map_data *map)
-{
-	size_t	i;
+// t_map_status	core_map_parser(t_map_data *map)
+// {
+// 	size_t	i;
 
-	i = 0;
-	while (i < map->map_lines_number)
-	{
-		if (is_empty_line(map->map_array[i]) == true)
-			return (INVALID_MAP);
-		else if (is_open_line(map->map_array[i]) == true)
-			return (INVALID_MAP);
-		++i;
-	}
-	return (VALID_MAP);
-}
+// 	i = 0;
+// 	while (i < map->map_lines_number)
+// 	{
+// 		if (is_empty_line(map->map_array[i]) == true)
+// 			return (INVALID_MAP);
+// 		else if (is_open_line(map->map_array[i]) == true)
+// 			return (INVALID_MAP);
+// 		++i;
+// 	}
+// 	return (VALID_MAP);
+// }
